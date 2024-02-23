2ce83b92e4fe5f9205a5d1eab7399419c2c8e99e
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-02-23 17:06:50 +0530

    final output sucess

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import ShortContainer from "./components/ShortContainer";
+
+function App() {
+  return (
+    <div>
+      <main className="main">
+        <ShortContainer />
+      </main>
+    </div>
+  );
+}
+
+export default App;
